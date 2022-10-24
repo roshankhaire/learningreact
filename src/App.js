bbd2cb13d3228e70e2cb9864563535769ac556d0
roshankhaire <roshankhaire1992@gmail.com>
2022-10-24 13:58:47 +0530

    props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const expenses=[
+    {id:"e1",
+    title:"Car Insurance" ,
+    amount:267.97,
+    date:new Date(2022,2,10),
+    locationOfExpenduture:"money"
+  },
+    {id:"e2",
+    title:"Bike Insurance" ,
+    amount:250.97,
+    date:new Date(2022,2,22)
+  },
+    {id:"e3",
+    title:"Health Insurance" ,
+    amount:244.97,
+    date:new Date(2022,2,10)
+  },
+  ];
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +36,11 @@ function App() {
         >
           Learn React
         </a>
-        <ExpenseItem></ExpenseItem>
-      </header>
+        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+       
+        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+    </header>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
 import './ExpenseItem.css'
-function ExpenseItem(){
+function ExpenseItem(props){
     const expenseDate=new Date(2022,2,10);
     const expenseTitle="Car Insurance"
     const expenseAmount=294.67
     return (<div>
-        <div className="Expense-item">{expenseDate.toDateString()}</div>
+        <div className="Expense-item">{props.date.toDateString()}</div>
         <div>
-            <h2 className="Expense-item__discription">{expenseTitle}</h2>
-            <div className="Expense-item__price">${expenseAmount}</div>
+            <h2 className="Expense-item__discription">{props.title}</h2>
+            <div className="Expense-item__price">${props.amount}</div>
+            <div className="Expense-item__location">${props.locationOfExpenduture}</div>
         </div>
     </div>)
 }
