47af4caf43f2b2e67b1318b5997fd5942a265857
roshankhaire <roshankhaire1992@gmail.com>
2022-10-24 17:40:42 +0530

    datemonthyear

--- src/App.js
+++ src/App.js
@@ -36,10 +36,10 @@ function App() {
         >
           Learn React
         </a>
-        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
        
         <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem> 
     </header>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,20 @@
 import './ExpenseItem.css'
 function ExpenseItem(props){
-    const expenseDate=new Date(2022,2,10);
-    const expenseTitle="Car Insurance"
-    const expenseAmount=294.67
+const month=props.date.toDateString('en-US',{month:'long'})
+const day=props.date.toDateString('en-US',{day:'2-digit'})
+const year=props.date.getFullYear()
     return (<div>
-        <div className="Expense-item">{props.date.toDateString()}</div>
+        <div className="Expense-item">
+            
+             <div>{month}</div>
+            <div>{day}</div>
+            <div>{year}</div>  
+            
+        </div>
         <div>
             <h2 className="Expense-item__discription">{props.title}</h2>
             <div className="Expense-item__price">${props.amount}</div>
-            <div className="Expense-item__location">${props.locationOfExpenduture}</div>
+           
         </div>
     </div>)
 }
